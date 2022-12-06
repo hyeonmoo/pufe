9e6df7d8787fd9705c59f7c697e8127db7eeca38
강현무 <hyeonm00@naver.com>
2022-12-06 11:43:13 +0900

    22-12-06 찐막

--- PutOnFe/src/main/java/com/putupiron/pufe/Ctrl_Rec_Comment.java
+++ PutOnFe/src/main/java/com/putupiron/pufe/Ctrl_Rec_Comment.java
@@ -7,20 +7,19 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.putupiron.pufe.dao.Rec_CommentService;
 import com.putupiron.pufe.dao.UserDao;
 import com.putupiron.pufe.dto.Rec_Comment;
 import com.putupiron.pufe.dto.User;
 
-@Controller
+@RestController
 public class Ctrl_Rec_Comment {
 
 	@Autowired Rec_CommentService service;
@@ -33,7 +32,6 @@ public class Ctrl_Rec_Comment {
 		return user;
 	}
 	// 지정된 게시물의 모든 댓글을 가져오는 메서드
-	@ResponseBody
 	@GetMapping("/comments")
 	public ResponseEntity<List<Rec_Comment>> list(Integer rec_num) {
 		List<Rec_Comment> list = null;
@@ -45,11 +43,11 @@ public class Ctrl_Rec_Comment {
 		}
 	}
 	// 댓글을 등록하는 메서드
-	@ResponseBody
 	@PostMapping(value="/comments", produces="application/text;charset=utf-8")
 	public ResponseEntity<String> write(@RequestBody Rec_Comment dto, Integer rec_num, HttpSession session) {
 		try {
 			User user = navBar(session);
+			if(user==null) throw new Exception("로그인 후 댓글작성이 가능합니다.");
 			dto.setUser_email(user.getUser_email());
 			dto.setRec_num(rec_num);
 			int cnt = service.write(dto);
@@ -61,19 +59,15 @@ public class Ctrl_Rec_Comment {
 	}
 
 	// 지정된 댓글을 삭제하는 메서드
-	@ResponseBody
 	@DeleteMapping(value="/comments/{rec_com_num}", produces="application/text;charset=utf-8")
 	public ResponseEntity<String> remove(@PathVariable Integer rec_com_num, Integer rec_num, HttpSession session) {
 		try {
 			User user = navBar(session);
 			int rowCnt = service.remove(rec_com_num, rec_num, user.getUser_email());
-			if (rowCnt != 1) {
-				throw new Exception("댓글 삭제 실패");
-			}
+			if (rowCnt != 1) throw new Exception("댓글 삭제 실패");
 			return new ResponseEntity<>("댓글을 삭제했습니다.", HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
-
 	}
 }
